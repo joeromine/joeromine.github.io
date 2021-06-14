2ee1683e0883e1e9af52382c284fba63c05faeb9
Joe Romine <joe.romine@ufl.edu>
2021-06-13 21:39:36 -0700

    Delete script directory

--- script/script.js
+++
@@ -1,8 +0,0 @@
-var burger = document.querySelector('.burger');
-burger.addEventListener('click',()=>{
-    document.querySelector('.fa-bars').classList.toggle('fa-rotate-90');
-    document.querySelectorAll('.social-media').forEach(e => e.classList.toggle('header-icon'));
-    document.querySelector('.fa-bars').classList.toggle('fa-rotate-90');
-    document.querySelector('.connect').classList.toggle('hidden');
-
-})
